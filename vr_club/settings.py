502a368b6d1f05466f81d1162e4a45fef1be2aa0
RezenkovD <rezenkovdmitro@gmail.com>
2023-06-09 16:55:31 +0300

    feat: add static folder

--- vr_club/settings.py
+++ vr_club/settings.py
@@ -128,8 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "whole-static")
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "whole-media")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
