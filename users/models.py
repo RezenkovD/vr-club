73c2af4ca3689a97761ebd5f7c89d1c8a629aaa3
RezenkovD <rezenkovdmitro@gmail.com>
2023-06-14 23:11:19 +0300

    feat: add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class User(models.Model):
+    email = models.EmailField(max_length=256, null=False, unique=True)
+    first_name = models.CharField(max_length=32, null=False)
+    last_name = models.CharField(max_length=32, null=False)
+    password = models.CharField(max_length=32, null=False)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return "{0} {1}".format(self.last_name, self.first_name)
