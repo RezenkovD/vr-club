2c1389e734a52f72ba43b28655bae71ae49c5791
RezenkovD <rezenkovdmitro@gmail.com>
2023-06-21 14:45:21 +0300

    feat: whitenoise docs

--- vr_club/wsgi.py
+++ vr_club/wsgi.py
@@ -14,6 +14,7 @@ from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vr_club.settings")
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
