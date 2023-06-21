f6abd8f3c7bc6a04c1589b98376fa8c974ff7609
RezenkovD <rezenkovdmitro@gmail.com>
2023-06-21 16:23:10 +0300

    update wsgi

--- vr_club/wsgi.py
+++ vr_club/wsgi.py
@@ -8,15 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-from pathlib import Path
 
 from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vr_club.settings")
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
-application.add_files(os.path.join(BASE_DIR, "vr_club_app/templates/"))
