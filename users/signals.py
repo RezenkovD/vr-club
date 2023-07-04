33638d1faef205770a1f4c1c359c42bb30ff48c3
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-04 22:49:22 +0300

    fix: fix signal for create profile

--- users/signals.py
+++ users/signals.py
@@ -2,16 +2,14 @@ from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Profile, VISITOR
+from django.core.exceptions import ObjectDoesNotExist
 
 User = get_user_model()
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
         Profile.objects.create(user=instance, role=VISITOR)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
