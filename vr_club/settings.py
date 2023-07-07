a1e49edfd06a3dea71d278c6e3c18e0c43421be9
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-07 13:19:20 +0300

    cd: setup google

--- vr_club/settings.py
+++ vr_club/settings.py
@@ -35,9 +35,9 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
+CSRF_TRUSTED_ORIGINS = ["https://vr-club.one", "https://www.vr-club.one"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-USE_X_FORWARDED_HOST = env("USE_X_FORWARDED_HOST")
+USE_X_FORWARDED_HOST = True
 # Application definition
 
 INSTALLED_APPS = [
