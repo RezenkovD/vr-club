97eb19b027d54d333251c2b58f7ff64a7b7e37a0
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-13 12:00:32 +0300

    feat: add sign-up page

--- vr_club/settings.py
+++ vr_club/settings.py
@@ -121,7 +121,7 @@ ROOT_URLCONF = "vr_club.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -198,6 +198,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "vr_club_app/templates/"),
     os.path.join(BASE_DIR, "vr_club_site/templates/"),
+    os.path.join(BASE_DIR, "users/templates/"),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
