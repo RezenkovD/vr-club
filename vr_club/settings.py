860646e66f3419b47a1b061812a3e91a7bb12d9f
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-17 15:20:19 +0300

    feat: add uk message error

--- vr_club/settings.py
+++ vr_club/settings.py
@@ -180,7 +180,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "uk"
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 TIME_ZONE = "UTC"
 
