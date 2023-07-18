61e024f1e214429f61f948e2acee7a8929b3b339
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-18 10:41:33 +0300

    feat: add google auth redirect

--- vr_club/settings.py
+++ vr_club/settings.py
@@ -65,7 +65,7 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
-
+SOCIALACCOUNT_LOGIN_ON_GET = True
 SITE_ID = 1
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
