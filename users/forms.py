7e399011d143c1edd0abcf44c6d316bc0a4ec263
Дмитро Рєзєнков <108422398+RezenkovD@users.noreply.github.com>
2023-07-18 10:13:19 +0300

    Feat/sign up page (#20)
    * feat: add sign-up page
    
    * feat: add button ащк bisibility
    
    * feat: add login page
    
    * fix: add login page
    
    * feat: add uk message error
    
    * feat: add eye
    
    * fix: fix for two pswd forms
    
    * feat: add valid errors
    
    * style: black formatting
    
    * test: comment test

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from allauth.account.forms import SignupForm
+from allauth.account.forms import SignupForm, LoginForm
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -104,5 +104,16 @@ class SignInForm(forms.Form):
 
 
 class CustomSignupForm(SignupForm):
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
+    first_name = forms.CharField(max_length=32, required=True, label="Name")
+
+    def __init__(self, *args, **kwargs):
+        super(CustomSignupForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input-field", "placeholder": ""})
+
+
+class CustomLoginForm(LoginForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input-field", "placeholder": ""})
--- vr_club/settings.py
+++ vr_club/settings.py
@@ -102,7 +102,10 @@ ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 LOGIN_REDIRECT_URL = "/users/"
 LOGOUT_REDIRECT_URL = "/users/"
-ACCOUNT_FORMS = {"signup": "users.forms.CustomSignupForm"}
+ACCOUNT_FORMS = {
+    "signup": "users.forms.CustomSignupForm",
+    "login": "users.forms.CustomLoginForm",
+}
 
 
 MIDDLEWARE = [
@@ -121,7 +124,7 @@ ROOT_URLCONF = "vr_club.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -177,7 +180,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "uk"
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, "locale"),
+]
 
 TIME_ZONE = "UTC"
 
@@ -198,6 +204,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "vr_club_app/templates/"),
     os.path.join(BASE_DIR, "vr_club_site/templates/"),
+    os.path.join(BASE_DIR, "users/templates/"),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
