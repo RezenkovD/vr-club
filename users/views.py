d00b0348f6653e4dae7c5fbeb8398786633bf2aa
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-26 11:30:48 +0300

    ref: setup base.html

--- users/views.py
+++ users/views.py
@@ -9,6 +9,14 @@ from .forms import SignInForm, SignUpForm
 from .models import VISITOR, Profile
 
 
+def base(request):
+    signup_url = reverse("account_signup")
+    login_url = reverse("account_login")
+    return render(
+        request, "base/base.html", {"signup_url": signup_url, "login_url": login_url}
+    )
+
+
 def home(request):
     return render(request, "home.html")
 
