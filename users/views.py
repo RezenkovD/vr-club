5de66bf85d2d0cb43783d8cda56e2e6dcaf2dcae
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-26 12:37:23 +0300

    fix: add reverse (flake8)

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, reverse
 
 from .forms import SignInForm, SignUpForm
 from .models import VISITOR, Profile
