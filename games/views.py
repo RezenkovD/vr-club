9cda4b757c06fe246eead515d2e29fce33c4d2e7
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-27 18:58:20 +0300

    feat: add games page

--- games/views.py
+++ games/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+
+from .models import Games
 
 # Create your views here.
+
+
+def games(request):
+    signup_url = reverse("account_signup")
+    login_url = reverse("account_login")
+    games = Games.objects.all()
+    return render(
+        request,
+        "games/games.html",
+        {"signup_url": signup_url, "login_url": login_url, "games": games},
+    )
