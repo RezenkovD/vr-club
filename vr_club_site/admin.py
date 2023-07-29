2e37de5f5c57e9157641c83f1aa1d6b129f1f0ca
RezenkovD <rezenkovdmitro@gmail.com>
2023-07-29 11:04:43 +0300

    feat: add sessionseats to admin

--- vr_club_site/admin.py
+++ vr_club_site/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import BookingTime, Booking
+from .models import BookingTime, Booking, SessionSeats
 
 # Register your models here.
 
 
 admin.site.register(BookingTime)
 admin.site.register(Booking)
+admin.site.register(SessionSeats)
