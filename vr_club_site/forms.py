83ba47236fac662da8d9049a0f07144eda9d1755
Дмитро Рєзєнков <108422398+RezenkovD@users.noreply.github.com>
2023-07-31 12:37:58 +0300

    Feat/booking system
    * feat: update booking model
    
    * feat: add date to bookingtime
    
    * feat: model for set session seats
    
    * feat: add sessionseats to admin
    
    * feat: add booking form
    
    * feat: form processing on the server
    
    * fix: fix template_name
    
    * feat: add client side booking

--- vr_club_site/forms.py
+++ vr_club_site/forms.py
@@ -1,10 +1,36 @@
 from django import forms
+from phonenumber_field.formfields import PhoneNumberField as PhoneNumberFormField
+
 from .models import BookingTime
 
 
 class BookingForm(forms.Form):
+    name = forms.CharField(
+        max_length=32,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+    )
+    email = forms.EmailField(
+        required=True,
+        max_length=64,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+    )
+    phone_number = PhoneNumberFormField(
+        widget=forms.TextInput(attrs={"class": "form-control"}), required=False
+    )
+    number_of_people = forms.IntegerField(min_value=1)
     comment = forms.CharField(
-        max_length=128, widget=forms.TextInput(attrs={"class": "form-control"})
+        max_length=256,
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+        required=False,
     )
     slots = forms.MultipleChoiceField(
         choices=BookingTime.TIME_CHOICES,
--- vr_club_site/models.py
+++ vr_club_site/models.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from django.contrib.auth.models import User
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
@@ -13,21 +16,41 @@ class BookingTime(models.Model):
         (OUTDATED, "Outdated"),
     )
     TIME_CHOICES = [
-        (f"{hour}-{hour + 1}", f"{hour}.00-{hour + 1}.00") for hour in range(13, 21)
+        (f"{hour}-{hour + 1}", f"{hour}.00-{hour + 1}.00") for hour in range(12, 22)
     ]
     time = models.CharField(max_length=5, choices=TIME_CHOICES, null=False)
     status = models.CharField(
         max_length=2, choices=STATUS_CHOICES, null=False, default=ACTUAL
     )
+    date = models.DateField(default=date.today)
 
     def __str__(self):
         return "{0} | {1}".format(self.time, self.status)
 
 
 class Booking(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    comment = models.CharField(max_length=128, null=True)
+    id = models.BigAutoField(
+        auto_created=True,
+        primary_key=True,
+        serialize=False,
+        verbose_name="ID",
+    )
+    name = models.CharField(max_length=32, null=False, default="vrclub")
+    email = models.EmailField(max_length=64, null=False, default="vrclub@gmail.com")
+    phone_number = PhoneNumberField(
+        null=True, blank=True, unique=False, default="+380000000000"
+    )
+    comment = models.CharField(max_length=256, null=True)
+    number_of_people = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     time = models.ManyToManyField(BookingTime)
 
     def __str__(self):
-        return self.user.username
+        return self.email + " " + str(self.price)
+
+
+class SessionSeats(models.Model):
+    number = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return "Сесійних місць: " + str(self.number)
